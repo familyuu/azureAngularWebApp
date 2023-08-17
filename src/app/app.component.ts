fb8b8c98f07f9a5c7c0e60638e5e246bbda55c05
luka <familyuu@163.com>
2023-08-17 16:37:27 +0800

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  templateUrl: './app.component.html',
 })
 export class AppComponent {
   value = 'World';
+  response: any;
+
+  constructor(private http: HttpClient) {}
+
+  test_click() {
+    this.http.get<any>('https://api.publicapis.org/entries').subscribe(
+      (res) => {
+        this.response = res;
+      },
+      (error) => {
+        this.response = error.message ? error.message : "Error"
+      }
+    );
+  }
 }
